118797425e62e3851dd56fb87d30c5c5eb240ccc
icaruswu <icaruswu66@qq.com>
2021-02-04 18:47:54 +0800

    add goroutine monitor

--- myMutex/monitor.go
+++ myMutex/monitor.go
@@ -12,14 +12,21 @@ import (
 var readValue = make(chan int)
 var writeValue = make(chan int)
 
+//set函数的目的是共享变量的值
 func set(newValue int) {
 	writeValue <- newValue
 }
 
+//read1函数的目的是读取已保存变量的值
 func read1() int {
 	return <-readValue
 }
 
+/*
+	当有读请求时,read1()函数从由monitor()函数控制的readValue管道读取数据.返回保存在value变量中的当前值
+	若想修改存储值,就是调用set函数,但是从wqriteValue里拿数据也是由select语句处理
+	结果就是,不使用monitor()函数,没人可以处理value共享变量
+*/
 func monitor() {
 	var value int
 	for {
