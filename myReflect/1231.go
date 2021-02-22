b7637649e9bd2bb39190415d4492852b4a0bb8aa
icaruswu <icaruswu66@qq.com>
2021-02-22 16:18:47 +0800

    reflect

--- myReflect/1231.go
+++ myReflect/1231.go
@@ -5,39 +5,35 @@ import (
 	"reflect"
 )
 
-func main()  {
+func main() {
 	var shit = []interface{}{
-		0:A,
-		1:B,
-		2:C,
+		0: A,
+		1: B,
+		2: C,
 	}
 
-	for k,y := range shit{
-		fmt.Println("K是",k)
-
+	for _, y := range shit {
 		valueOf := reflect.ValueOf(y)
-		t := valueOf.Type()
-		in := t.In(0)
-		out := t.Out(0)
+		t := valueOf.Type() //例如func(int, float64) int
+		in := t.In(0)       //函数A的第0的参数的类型
+		out := t.Out(0)     //函数A的第0个返回值类型
 
-		fmt.Println("in是",in)		//输出in是int
-		fmt.Println("out是",out)	//输出out是int
+		fmt.Println("in是", in)   //输出in是int
+		fmt.Println("out是", out) //输出out是int
 	}
 
-	shit[0] = B(1)
-	fmt.Println(shit[0])
-
-
+	shit[0] = B(1) //此时有入参,因此是调用函数
+	fmt.Println("打印一下: ", shit[0])
 }
 
-func A(i int) int {
-	return i+10
+func A(i int, j float64) int {
+	return i + 10
 }
 
 func B(i int) int {
-	return i+10
+	return i + 10
 }
 
 func C(i int) int {
-	return i+10
+	return i + 10
 }
