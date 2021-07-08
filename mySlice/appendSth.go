e822fdf588b9de0429fd0f08d7a7520c151e7a3b
icaruswu <icaruswu66@qq.com>
2021-07-08 17:57:49 +0800

    slice append

--- mySlice/appendSth.go
+++ mySlice/appendSth.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+/*
+	若一开始a的len置为0的话,最后出现的情况是cap3里都有值,但却因为len是0,而打印什么都没有.
+	解析:
+		起初a的len与cap都是3; b的len是0,cap是3.
+		第8行,append操作先将数据存入b指针实体中, 然后再去改变b的len值.及先执行append右边,最后执行左边的b=
+		19行中,相当于b的底层指针数据成功append进去了,且a的len是3,所以a指向的实体能成功显示出来.
+				对于b而言,虽然指针实体的数据成功变了,但是他的len还是2,没能改变.
+*/
 func main() {
 	a := make([]int, 3) //注意此处a的len与cap都是3
 	b := a[:0]          //b的len是0,cap是3, 此刻b指针与a的指针指向同一个地方
@@ -13,11 +21,16 @@ func main() {
 	fmt.Println(a)      // [1,2,3]	-> a成功追加c
 }
 
+
 /*
-	若一开始a的len置为0的话,最后出现的情况是cap3里都有值,但却因为len是0,而打印什么都没有.
-	解析:
-		起初a的len与cap都是3; b的len是0,cap是3.
-		第8行,append操作先将数据存入b指针实体中, 然后再去改变b的len值.及先执行append右边,最后执行左边的b=
-		11行中,相当于b的底层指针数据成功append进去了,且a的len是3,所以a指向的实体能成功显示出来.
-				对于b而言,虽然指针实体的数据成功变了,但是他的len还是2,没能改变.
+	看go/src/runtime/slice.go -->growslice
+	append后的cap首先是9,走case et.size == sys.PtrSize的roundupsize函数, 注意:sys.PtrSize应该是8个字节的意思
+	9*8=72, divRoundUp得到9, size_to_class8[9]=7, class_to_size[7]=80, 最后80/8等于10字节
 */
+func another()  {
+	arr1 := [4]int{1,2,3,4}
+	slice1 := arr1[:3]
+	fmt.Println(slice1,len(slice1),cap(slice1))			//[1 2 3] 3 4
+	slice1 = append(slice1,5000,6000,7000,8000,9000,10000)
+	fmt.Println(slice1,len(slice1),cap(slice1))			//[1 2 3 5000 6000 7000 8000 9000 10000] 9 10
+}
--- mySlice/simple.go
+++ mySlice/simple.go
@@ -17,29 +17,32 @@ type slice struct {
 	所以v的值发生改变了，注意这里v的值是底层数组的头节点地址.结论是：v与nums所指向的值发生了改变。
 */
 func bad() {
-	nums := []int{1, 2, 3}
+	nums := []int{1,2,3}
 	badAppendNum(nums)
-	fmt.Println("bad外：", nums)
+	fmt.Println("bad外：",nums)
 }
 
-func badAppendNum(v []int) {
-	v = append(v, 4)
-	fmt.Println("bad里：", v)
+func badAppendNum(v []int)  {
+	v = append(v,4)
+	fmt.Println("bad里：",v)
 }
+
 
 /*
 	对原nums进行直接操作，而没有copy nums
 */
-func good() {
-	nums := []int{1, 2, 3}
+func good()  {
+	nums := []int{1,2,3}
 	goodAppendNum(&nums)
-	fmt.Println("good外：", nums)
+	fmt.Println("good外：",nums)
 }
 
-func goodAppendNum(v *[]int) {
-	*v = append(*v, 4)
-	fmt.Println("good里:", *v)
+func goodAppendNum(v *[]int)  {
+	*v = append(*v,4)
+	fmt.Println("good里:",*v)
 }
+
+
 
 /*
 	这里先初始化一个len为3，cap为4的slice，然后直接copy这个slice去append，
@@ -47,32 +50,24 @@ func goodAppendNum(v *[]int) {
 	因为我们直接操作修改了slice底层的array指针数组，主函数直接打印v没有变化是因为它此时的len还是3没有改变
 	而cap是4，说明还有一个空间是可以去append，而不用扩容重新指向新的slice
 */
-func something() {
-	v := make([]int, 3, 4)
-	v[0] = 1
-	v[1] = 2
-	v[2] = 3
-	fmt.Println(v, len(v), cap(v))
+func something()  {
+	v := make([]int,3,4)
+	v[0]=1
+	v[1]=2
+	v[2]=3
+	fmt.Println(v,len(v),cap(v))
 	fmt.Println(v[:4])
 
 	change(v)
-	fmt.Println(v, len(v), cap(v))
+	fmt.Println(v,len(v),cap(v))
 	fmt.Println(v[:4])
 }
 
-func change(v []int) {
-	v = append(v, 4)
-	fmt.Println(v, len(v), cap(v))
+func change(v []int)  {
+	v = append(v,4)
+	fmt.Println(v,len(v),cap(v))
 }
 
-func main() {
+func main()  {
 	something()
-}
-
-func interesting() {
-	a := []int{1, 2, 3, 4, 5}
-	b := a[:2]   //表示[1,2],且len为2,cap是5
-	c := a[:2:3] //表示[1,2],且len为2,cap为3
-	fmt.Println(b, len(b), cap(b))
-	fmt.Println(c, len(c), cap(c))
 }
